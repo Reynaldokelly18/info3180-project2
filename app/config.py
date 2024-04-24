ca2c4a796a4bb11e14186c5ccf9b563ee8de6eb5
Michael Benjamin <mbenji99@gmail.com>
2024-04-24 13:08:58 -0500

    Edited to cover up to question 3

--- app/config.py
+++ app/config.py
@@ -3,10 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env if it exists.
 
+
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', './uploads')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI is None:
+        SQLALCHEMY_DATABASE_URI = 'postgresql://project2_user:Monday#123@localhost/project2'
+    else:
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import LoginView from '../views/LoginView.vue'
+import ExploreView from '../views/ExploreView.vue'
+import LogoutView from '../views/LogoutView.vue'
+import NewPostView from '../views/NewPostView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +16,33 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/explore',
+      name: 'explore',
+      component: ExploreView
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: LogoutView
+    },
+    {
+      path: '/posts/new',
+      name: 'post',
+      component: NewPostView
+    },
+    {
+      path: '/users/:user_id',
+      name: 'profile',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
