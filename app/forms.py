dc83ab8264fb973db643552b58011a3435f3d99a
reynaldokelly18 <reynaldokelly8@gmail.com>
2024-04-26 20:38:28 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,27 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FileField, PasswordField, EmailField
+from wtforms.validators import InputRequired, Email
+from flask_wtf.file import FileField,FileRequired,FileAllowed
+
+
+class Register(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired(), Email()])
+    location = StringField('Location', validators=[InputRequired()])
+    biography = TextAreaField('Biography', validators=[InputRequired()])
+    photo = FileField('Upload Photo', validators=[
+        FileRequired(message='Please upload profile photo'),
+        FileAllowed(['jpg', 'png'], message='Only JPEG and PNG images are allowed.')
+    ])
+
+
+
+class NewPost(FlaskForm):
+    photo = FileField('Upload Poster', validators=[
+        FileRequired(message='Please upload movie poster'),
+        FileAllowed(['jpg', 'png'], message='Only JPEG and PNG images are allowed.')
+    ])
+    caption = TextAreaField('Caption', validators=[InputRequired()])
